fadc61117dc09797774d9b641cd07c9945105d88
johnverz22 <johnny.verzola@lorma.edu>
2026-01-22 14:34:40 +0800

    week 2 codes

---
+++ CLASS_DEMO_CODES/week2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle myCircle = new Circle(50);
+        System.out.println("The area of the circle with radius " +
+            myCircle.radius + " is " + myCircle.getArea()
+
+        );
+    }
+    
+}
