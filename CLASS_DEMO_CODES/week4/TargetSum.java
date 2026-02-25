22628f2ac7afa38262b205d4ef07006a47b7a4f6
johnverz22 <johnny.verzola@lorma.edu>
2026-02-25 12:09:20 +0800

    week 7

---
+++ CLASS_DEMO_CODES/week4/TargetSum.java
@@ -0,0 +1,26 @@
+public class TargetSum {
+    public static void main(String[] args) {
+        for(String arg : args){
+            System.out.println(arg);
+        }
+
+
+    //     int[] nums = {1, 2, 3, 4, 5};
+    //     int target = 5;
+    //     boolean found = false;
+    //     for(int i = 0; i < nums.length; i++){
+    //         for(int j = 0; j < nums.length; j++){
+    //             if (i == j) continue;
+
+    //             if(nums[i] + nums[j] == target){
+    //                 System.out.printf("[%d,%d]", i, j);
+    //                 found = true;
+    //                 break;
+    //             }
+    //         }
+    //         if(found) break;
+
+    //     }
+    // 
+    }
+}
